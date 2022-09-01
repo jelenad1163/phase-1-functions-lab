1ee8234f4edf335f3f57a27dcd6c5f2d0df19c1b
Jelena Djurdjevic <+>
2022-09-01 10:56:30 -0600

    commit tree 342229dcda70e7643bf0c19f56a00b3f6723c5b7

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    if (pickUp<42){
+        return(42-pickUp);
+    }
+    else {
+        return(pickUp-42);
+    }
+}
+function distanceFromHqInFeet(pickUp){
+    return distanceFromHqInBlocks(pickUp)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    if(destination> start){
+        return (destination-start)*264;
+    }
+    else
+    return(start-destination)*264;
+
+}
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance<= 400){
+        return 0
+    }
+    if (distance>=400 && distance<=2000){
+        return (distance -400)*.02;
+    }
+    else if(distance>=2000 && distance <=2500){
+        return 25;
+    }
+    else if (distance > 2500){
+        return "cannot travel that far";
+    }
+    
+    }
